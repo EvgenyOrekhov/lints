0a4134001c51ad89b76884ba17415f5b071648aa
Evgeny Orekhov <EvgenyOrekhov@gmail.com>
2017-02-28 14:58:24 +0300

    Add markdownlint

---
+++ src/linters/markdownlint.js
@@ -0,0 +1,45 @@
+/*jslint node, es6, maxlen: 80 */
+
+"use strict";
+
+const markdownlint = require("markdownlint");
+
+const Bluebird = require("bluebird");
+
+const markdownlintAsync = Bluebird.promisify(markdownlint);
+
+module.exports = function makeLinter({promisedOptions}) {
+    return function lint({promisedFile}) {
+        return Bluebird
+            .props({
+                options: promisedOptions,
+                file: promisedFile
+            })
+            .then(function ({options, file}) {
+                return markdownlintAsync({
+                    strings: {data: file},
+                    config: options,
+                    resultVersion: 1
+                }).then(function ({data}) {
+                    return {
+                        linterName: "markdownlint",
+                        warnings: data.map(function ({
+                            lineNumber: line,
+                            errorDetail,
+                            ruleDescription,
+                            ruleAlias: ruleId
+                        }) {
+                            return {
+                                line,
+                                column: 0,
+                                message: errorDetail
+                                    ? `${ruleDescription} (${errorDetail})`
+                                    : ruleDescription,
+                                ruleId
+                            };
+                        })
+                    };
+                });
+            });
+    };
+};
---
+++ test/linters/markdownlint.js
@@ -0,0 +1,83 @@
+/*jslint node, es6, maxlen: 80 */
+
+"use strict";
+
+const {test} = require("tap");
+
+const makeLinter = require("../../src/linters/markdownlint");
+
+test("markdownlint", function (t) {
+    const lint = makeLinter({
+        promisedOptions: Promise.resolve()
+    });
+
+    return lint({
+        promisedFile: Promise.resolve("a")
+    }).then(
+        (result) => t.strictSame(
+            result,
+            {
+                linterName: "markdownlint",
+                warnings: [
+                    {
+                        line: 1,
+                        column: 0,
+                        message: "First line in file should be a top level "
+                                + "header",
+                        ruleId: "first-line-h1"
+                    }
+                ]
+            },
+            "should lint using markdownlint"
+        )
+    );
+});
+
+test("markdownlint", function (t) {
+    const lint = makeLinter({
+        promisedOptions: Promise.resolve()
+    });
+
+    return lint({
+        promisedFile: Promise.resolve(`# a
+
+`)
+    }).then(
+        (result) => t.strictSame(
+            result,
+            {
+                linterName: "markdownlint",
+                warnings: [
+                    {
+                        line: 3,
+                        column: 0,
+                        message: "Multiple consecutive blank lines "
+                                + "(Expected: 1; Actual: 2)",
+                        ruleId: "no-multiple-blanks"
+                    }
+                ]
+            },
+            "should use the \"errorDetail\" property if exists"
+        )
+    );
+});
+
+test("markdownlint", function (t) {
+    const lint = makeLinter({
+        promisedOptions: Promise.resolve({"first-line-h1": false})
+    });
+
+    return lint({
+        promisedFile: Promise.resolve(`a
+`)
+    }).then(
+        (result) => t.strictSame(
+            result,
+            {
+                linterName: "markdownlint",
+                warnings: []
+            },
+            "should use options"
+        )
+    );
+});
