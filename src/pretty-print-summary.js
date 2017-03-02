8b9866750da3191efb81c53da87da0f947371c66
Evgeny Orekhov <EvgenyOrekhov@gmail.com>
2017-03-02 21:05:40 +0300

    Add colors to the "Total warnings" message

--- src/pretty-print-summary.js
+++ src/pretty-print-summary.js
@@ -3,19 +3,32 @@
 "use strict";
 
 const wordwrap = require("wordwrap");
+const chalk = require("chalk");
 
 const maxLineLength = 80;
 
 const wrapTo80 = wordwrap(maxLineLength);
 
 module.exports = function prettyPrintSummary({numbers, usedLinters}) {
-    return usedLinters.length > 0
-        ? wrapTo80(`Used linters: ${usedLinters.join(", ")}
+    if (usedLinters.length === 0) {
+        return "Nothing to check\n";
+    }
+
+    const color = numbers.totalWarnings > 0
+        ? chalk.red
+        : chalk.green;
+
+    const totalWarningsMessage = color(
+        `Total warnings: ${numbers.totalWarnings}`
+    );
+
+    return wrapTo80(
+        `Used linters: ${usedLinters.join(", ")}
 
 Total files checked: ${numbers.totalFiles}
 Files with warnings: ${numbers.filesWithWarnings}
 
-Total warnings: ${numbers.totalWarnings}
-`)
-        : "Nothing to check\n";
+${totalWarningsMessage}
+`
+    );
 };
--- test/pretty-print-summary.js
+++ test/pretty-print-summary.js
@@ -5,6 +5,9 @@
 
 const {test} = require("tap");
 
+const chalk = require("chalk");
+const ansiStyles = require("ansi-styles");
+
 const prettyPrintSummary = require("../src/pretty-print-summary");
 
 test("prettyPrintSummary()", function (t) {
@@ -77,6 +80,58 @@ Total warnings: 8
 `,
         "should wrap lines to 80 characters"
     );
+
+    t.end();
+});
+
+test("prettyPrintSummary()", function (t) {
+    // eslint-disable-next-line fp/no-mutation
+    chalk.enabled = true;
+
+    const badResult = prettyPrintSummary({
+        numbers: {
+            totalFiles: 15,
+            filesWithWarnings: 4,
+            totalWarnings: 8
+        },
+        usedLinters: ["linterA", "linterB"]
+    });
+
+    t.strictSame(
+        badResult,
+        `Used linters: linterA, linterB
+
+Total files checked: 15
+Files with warnings: 4
+
+${ansiStyles.red.open}Total warnings: 8${ansiStyles.red.close}
+`,
+        "should use red color if there are warnings"
+    );
+
+    const goodResult = prettyPrintSummary({
+        numbers: {
+            totalFiles: 15,
+            filesWithWarnings: 0,
+            totalWarnings: 0
+        },
+        usedLinters: ["linterA", "linterB"]
+    });
+
+    t.strictSame(
+        goodResult,
+        `Used linters: linterA, linterB
+
+Total files checked: 15
+Files with warnings: 0
+
+${ansiStyles.green.open}Total warnings: 0${ansiStyles.green.close}
+`,
+        "should use green color if there are no warnings"
+    );
+
+    // eslint-disable-next-line fp/no-mutation
+    chalk.enabled = false;
 
     t.end();
 });
